021b9968b239272185943d08c6a8b8eecc3aa23b
Brian-T-Hart <hart.briant@gmail.com>
2020-11-29 01:44:39 -0800

    utilizing modules

--- JavaScript/Fetch/index.js
+++ JavaScript/Fetch/index.js
@@ -1,55 +1,6 @@
-const todosUrl = 'https://jsonplaceholder.typicode.com/todos';
-const usersUrl = 'users.json';
+// import './modules/users.js';
+import './modules/todos.js';
+import {publicMessage} from './modules/messages.js';
 
-//fetch the todos and add them to the DOM
-fetch(todosUrl)
-.then(response => response.json())
-.then(json => {
-    let data = json;
-    buildTodos(data);
-})
-
-//fetch users and add them to the DOM
-fetch(usersUrl)
-.then(response => response.json())
-.then(json => {
-    let data = json;
-    buildUsers(data);
-})
-
-function buildTodos(data) {
-    const todos = data;
-    var html = '<h2>Todo List</h2>';
-    html += '<ul id="todos">';
-    
-    for (i = 0; i < 10; i++) {
-        html += `<li>${todos[i].title}</li>`;
-    }
-
-    html += '</ul>';
-
-    let todosEl = document.getElementById('todos-container');
-    todosEl.innerHTML = html;
-}
-
-function buildUsers(data) {
-    const users = data.users;
-
-    var html = '<h2>Users Table</h2>';
-    html += '<table id="users">';
-    html += '<tr><th>Username</th><th>Email</th></tr>';
-    
-    for (i = 0; i < users.length; i++) {
-        html += '<tr><td>' + users[i].username + '</td><td>' + users[i].email + '</td></tr>';
-        // html += `<tr><td>${users[i].username}</td><td>${users[i].email}</td></tr>`;
-    }
-
-    html += '</table>';
-    
-    let usersEl = document.getElementById('users-container');
-    usersEl.innerHTML = html;
-}
-
-
-
-
+console.log(publicMessage);
+// console.log(secretMessage);
