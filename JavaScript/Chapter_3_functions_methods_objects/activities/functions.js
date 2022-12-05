47d8c1ab8d1299daf77669113ef8caa2322544a6
Brian-T-Hart <hart.briant@gmail.com>
2022-12-04 20:09:58 -0800

    added functions activities

---
+++ JavaScript/Chapter_3_functions_methods_objects/activities/functions.js
@@ -0,0 +1,34 @@
+/* Activity 1
+    Create a function to display an alert message.
+    The function needs one parameter named “message”.
+    Don’t forget to call the function.
+*/
+
+
+
+/* Activity 2
+    Create a function that takes in two numbers and returns their sum
+    Call the function within a console.log() to display the number
+*/
+
+
+
+/* Activity 3
+    Create a global variable called pageHits and set the initial value equal to zero
+    Create an IIFE (Immediately Invoked Function Expression) that increments the value by 1
+    Write a message to the screen that contains the number of pageHits
+*/
+
+
+
+/* Activity 4
+    - Create a constructor function named person that will be used to create instances of a person
+    - The constructor should set the following properties
+        * firstName
+        * lastName
+        * age
+    - The constructor should have a method named birthday that increases the person’s age by 1 year
+    - It should also have a method called getAge which returns the person’s age
+    - Call the birthday method
+    - Console log
+*/
