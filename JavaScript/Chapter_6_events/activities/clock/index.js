f23f2449daf1422fe2886370d301788a306eb9df
Brian-T-Hart <hart.briant@gmail.com>
2023-08-20 12:53:46 -0700

    added clock activity

---
+++ JavaScript/Chapter_6_events/activities/clock/index.js
@@ -0,0 +1,10 @@
+/*
+    Activity 1 - Clock
+        Create a simple clock that displays hours, minutes and seconds (ex. 7:45:00 PM)
+        // create an h1 element for the time
+        // give the element an id of 'clock'
+        // set the innerText = to new Date().localTimeString()
+        // append the element to the body
+        // create a function that updates the clock element with the current time
+        // run the function every second (use setInterval)
+*/
