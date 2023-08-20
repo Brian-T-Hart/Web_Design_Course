7cb0ab606ef37b9e2805d9af81e34b098b87f358
Brian-T-Hart <hart.briant@gmail.com>
2023-08-20 12:46:04 -0700

    Added decisions and loops activities

---
+++ JavaScript/Chapter_4_decisions_loops/activities/index.js
@@ -0,0 +1,15 @@
+const randomNumber = Math.floor(Math.random() * 10);
+
+/* Activity 1
+    Create a function called randomWinner that takes a random number.
+    If the number is greater than 6, alert ('You Win').
+    Else, alert ('Sorry, try again');
+*/
+
+const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
+
+/* Activity 2
+    Create a for loop that loops through the array of colors
+    and logs them to the console.
+*/
+
